8b868899d64ff410b43e73d9247c2bf7e8e6c7d8
sushilj33 <sushiljagtap96k@gmail.com>
2023-03-17 11:01:00 +0530

    installed sass, created board and square components

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const Square = ({value}) => {
+
+  return (
+    <button type='button' className='square'>
+      {value}
+    </button>
+    
+  );
+};
+
+export default Square;
